7aa4fc782c4073efd021015400b209acdb89fa0b
Margaret Mwathi <mwathiann201@yahoo.com>
2023-02-18 03:15:47 -0800

    lowercase alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Lowercase alphabet
+ * Return: 0
+ */
+int main(void)
+{
+	char m;
+
+	m = 'a';
+	while
+		(m <= 'z')
+		{
+	printf("%c", m++);
+		}
+	printf("\n");
+	return (0);
+}
+
