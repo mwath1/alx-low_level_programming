6697afa6a408b50887aa9038460511e9e6eaddf3
Margaret Mwathi <mwathiann201@yahoo.com>
2023-02-18 04:40:20 -0800

    ommit 2 letter in alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Alphabet followed by new line
+ * Return: 0
+ */
+int main(void)
+{
+	char v;
+
+	v = 'a';
+	while
+		(v <= 'z')
+		{
+			putchar(v);
+			v++;
+		}
+	putchar('\n');
+	return (0);
+}
