aac5f1648c8115a2b148efc1dfedd9c6d6019147
Margaret Mwathi <mwathiann201@yahoo.com>
2023-02-21 09:42:54 -0800

    print x1o lowecase alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * main - print_x10 alphabet in lowercase
+ * print_alphabet_x10 - print_x10 alphabet in lowercase
  *
  * Return: 0
  */
@@ -10,7 +10,7 @@ void print_alphabet_x10(void)
 	int b;
 	char c;
 
-	for (b = 0; b <= 10; b++)
+	for (b = 0; b <= 9; b++)
 	{
 		for (c = 'a'; c <= 'z'; c++)
 		{
