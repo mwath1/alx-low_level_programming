25f999d333cd4109be503324b7f5563092a8985a
Margaret Mwathi <mwathiann201@yahoo.com>
2023-02-21 13:17:57 -0800

    computes the absolute value of an integer.

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,15 +3,12 @@
 /**
  * _abs - compute the absolute value of integer
  * @i: integer to be conveted
- * main - check the code
  * Return: absolute value of input number
  */
 int _abs(int i)
 {
-	if (i < 0)
-		i = i * -1;
-}
-int main(void)
-{
-	return (i);
+	if (i >= 0)
+		return (i);
+	else
+		return (-i);
 }
