2367d82fc525462f11f5786b76d3e58071e8faf2
Margaret Mwathi <mwathiann201@yahoo.com>
2023-02-21 12:44:08 -0800

    add 2 integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers.
+ * @n1: first integer
+ * @n2: second integer
+ *
+ * Return: result of the addition.
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * times_table - pint table from 0 -9
+ *
+ * Return: 0
+ */
+void times_table(void)
+{
+	int g, h, res;
+
+	for (g = 0; g <= 9; g++)
+	{
+		for (h = 0; h <= 9; h++)
+		{
+			res = (g * h);
+			if (h != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			else if (res >= 10)
+			{
+				_putchar((res / 10) + '0');
+				_putchar((res % 10) + '0');
+			}
+			else if (res < 10 && h != 0)
+			{
+				_putchar(' ');
+				_putchar((res % 10) + '0');
+			}
+			else
+				_putchar((res % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
