4782e7da006e2e02871e000ae5124ba0e846b089
Margaret Mwathi <mwathiann201@yahoo.com>
2023-02-21 06:45:45 -0800

    Print alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - print alphabet in lower case
+ *
+ * Return: 0
+ */
+void print_alphabet
+{
+	int p;
+	for (p = 'a'; p <= 'z'; p++)
+	{
+		_putchar(p);
+	}
+	_putchar('\n');
+	return (0);
+}
