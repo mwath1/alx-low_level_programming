56b93c54ea727e5f4ab88b3b7c4dc0757a43f367
Margaret Mwathi <mwathiann201@yahoo.com>
2023-02-21 11:28:15 -0800

    print last digit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - compute the absolute value of integer
+ * @i: integer to be conveted
+ * main - check the code
+ * Return: absolute value of input number
+ */
+int _abs(int i)
+{
+	if (i < 0)
+		i = i * -1;
+}
+int main(void)
+{
+	return (i);
+}
