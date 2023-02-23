16d1b8a86493acc7eeb705b75ba8aaf9b38b2f95
Margaret Mwathi <mwathiann201@yahoo.com>
2023-02-23 06:43:18 -0800

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * print_numbers - Print numbers from 0 to 9
+ *
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	char x;
+
+	x = 0;
+	for (x <= 9)
+	{
+		_putchar('x');
+		x++;
+	}
+	_putchar('\n');
+}
