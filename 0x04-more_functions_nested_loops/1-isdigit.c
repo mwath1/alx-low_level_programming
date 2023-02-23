536f7a60c925f5f945066e937965515e0129f9a2
Margaret Mwathi <mwathiann201@yahoo.com>
2023-02-23 05:53:30 -0800

    is upper and digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isdigit - print digit 0 to 9
+ * @c: character to check and print
+ * Return: 1 if c is digit , 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	c = 0;
+	c++;
+	if (c <= 9)
+		return (1);
+	else
+		return (0);
+}
+
