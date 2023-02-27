ca88110eed975c59a18152410dce906a15a99e2e
Margaret Mwathi <mwathiann201@yahoo.com>
2023-02-27 09:55:59 -0800

    copy paste

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,10 +13,10 @@ void puts_half(char *str)
 	len = 0;
 	for (i = 0; str[i] != '\0'; i++)
 		len++;
-	j = (len / 2);
+	k = (len / 2);
 	if ((len % 2) == 1)
-		j = ((len + 1) / 2);
-	for (i = j; str[i] != '\0'; i++)
+		k = ((len + 1) / 2);
+	for (i = k; str[i] != '\0'; i++)
 		_putchar(str[i]);
 	_putchar('\n');
 }
--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -17,7 +17,7 @@ void print_array(int *a, int n)
 	}
 	if (k == (n - 1))
 	{
-		printf("%d", n[n - 1]);
+		printf("%d", a[n - 1]);
 	}
 	printf("\n");
 }
