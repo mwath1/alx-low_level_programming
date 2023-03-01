9e8f652433892275d78943cc80ac1023d2cafa5f
Margaret Mwathi <mwathiann201@yahoo.com>
2023-03-01 09:50:31 -0800

    encodes a string into 1337.

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * leet - ecodes a string
+ * leet - ecodes a string to 1337
  * @n: input string
  *
  * Return: n
@@ -12,11 +12,11 @@ char *leet(char *n)
 	char s1[] = "aAeEoOtTlL";
 	char s2[] = "4433007711";
 
-	for (i =; n[i] != '\0'; i++)
+	for (i = 0; n[i] != '\0'; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			if(n[i] == s1)
+			if(n[i] == s1[j])
 			{
 				n[i] = s2[j];
 			}
