40a059e0f798013f3f9f6636b52c2d5665da51a9
Margaret Mwathi <mwathiann201@yahoo.com>
2023-03-09 07:41:01 -0800

    string length

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,12 +6,17 @@
  * Return: length of string
  */
 
+int _strlen_recursion(char *s);
+/* _strlen_recursion - function declaration */
+
 int _strlen_recursion(char *s)
 {
-	if (*s == '\0')
+	int len = 0;
+
+	if (*s)
 	{
-		return (0);
+		len = _strlen_recursion(s + 1);
+		return (len = len + 1);
 	}
-	s++;
-	return (_strlen_recursions(s + 1));
+	return (0);
 }
