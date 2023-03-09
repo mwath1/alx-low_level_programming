ba83b59fce10fb3fe98320b1259531f1b0e6efce
Margaret Mwathi <mwathiann201@yahoo.com>
2023-03-09 10:25:21 -0800

    prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -39,3 +39,27 @@ int prime_no(int num, int num2)
 		return (prime_no(num, num2 + 1));
 	}
 }
+
+/**
+ * is_prime_number - check for prime number
+ * @n: value to test
+ * Return: 1 if prime number,otherwise 0
+ */
+
+int is_prime_number(int n)
+{
+	int divisor = 2;
+
+	if (n <= 1)
+	{
+		return (0);
+	}
+	else if (n >= 2 && n <= 3)
+	{
+		return (1);
+	}
+	else
+	{
+		return (prime_no(n, divisor));
+	}
+}
