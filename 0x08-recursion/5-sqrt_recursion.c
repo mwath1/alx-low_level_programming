2e52d9b37956f6454d7d777616ee1762672609ba
Margaret Mwathi <mwathiann201@yahoo.com>
2023-03-09 08:20:32 -0800

    square root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,11 +1,23 @@
 #include "main.h"
 
-int actual_sqrt_recursion(int n, int i);
-
 /**
  * _sqrt_recursion - find square root of value
- * @n: value to be square rooted
+ * @m: value to be square rooted
+ * @i: root to be tested
  * Return: root of value n
+ */
+
+int actual_sqrt_recursion(int m, int i);
+/**
+ * actual_sqrt_recursion - function defination
+ * @n: value to be rooted
+ * Return: square root
+ */
+int _sqrt_recursion(int n);
+/**
+ * _sqrt_recursion - function defination
+ * @n: value to be rooted
+ * Return: square root
  */
 
 int _sqrt_recursion(int n)
@@ -13,21 +25,21 @@ int _sqrt_recursion(int n)
 	if (n < 0)
 		return (-1);
 	if (n == 0)
-		return (actual_sqrt_recursion(n, 0));
+		return (actual_sqrt_recursion(m, 0));
 }
 
 /**
  * actual_sqrt_recursion - recurs to find natural square root
- * @n: value to be rooted
+ * @m: value to be rooted
  * @i: iterator
  * Return: square root
  */
 
-int actual_sqrt_recursion(int n, int i)
+int actual_sqrt_recursion(int m, int i)
 {
-	if (i * i > n)
+	if (i * i > m)
 		return (-1);
-	if (i * i == n)
+	if (i * i == m)
 		return (i);
-	return (actual_sqrt_recursion(n, i + 1));
+	return (actual_sqrt_recursion(m, i + 1));
 }
