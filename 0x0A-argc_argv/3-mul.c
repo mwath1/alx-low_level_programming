519a85a6240cc24d9edd2b87010ad781395e576e
Margaret Mwathi <mwathiann201@yahoo.com>
2023-03-15 07:30:30 -0700

    multiplication

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - multiplies 2 numbers
+ * @argc: argument count
+ * @argv:argument vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0, j = 0;
+	if (argc == 3)
+	{
+		i = atoi(argv[1]);
+		j = atoi(argv[2]);
+		printf("%d\n", i * j);
+	}
+	else
+		printf("Error\n");
+	return (1);
+}
