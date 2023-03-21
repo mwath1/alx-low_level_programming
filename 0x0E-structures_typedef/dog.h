5411ddecfdcfa841bddaf1c79609e1a3a18f1e93
Margaret Mwathi <mwathiann201@yahoo.com>
2023-03-21 09:14:52 -0700

    function that prints

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,10 @@
 #ifndef DOG_H
 #define DOG_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
+
 struct dog
 {
 	char *name;
@@ -14,4 +18,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
 #endif
