2b91e3d5e8921aee45ee1a5d03afa3ebfbd4b73e
Margaret Mwathi <mwathiann201@yahoo.com>
2023-03-21 09:01:51 -0700

    Define a new type

--- dog.h
+++ dog.h
@@ -1,10 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog {
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
-}my_dog;
+};
 
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
