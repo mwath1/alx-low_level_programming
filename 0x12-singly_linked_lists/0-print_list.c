97a062d10b5f509ecc5d60070ec88a58facb8423
mwath1 <mwathiann201@yahoo.com>
2023-03-30 02:16:04 -0700

    prints all the elements of a list_t list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "list.h"
+
+/**
+ * size_t print_list - prints all elements in list
+ * @h: holds elements to be printed
+ * Return: no. of nodes
+ */
+
+size_t print_list(const list_t *h);
+{
+size_t s = 0;
+
+while (h)
+{
+	if (!h->str)
+		printf("[0](nil\n");
+	else
+		printf("[%u]%s\n", h->len, h->str);
+	h = h->next;
+	s++;
+}
+
+return (s);
+}
