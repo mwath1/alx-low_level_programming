569218dea096a8b9bf3baa432028a0096d89e389
mwath1 <mwathiann201@yahoo.com>
2023-04-03 11:14:15 -0700

    fix betty bugs

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -3,7 +3,8 @@
 /**
  * insert_nodeint_at_index - inserts a new node at a given position.
  * @head: holds address of list
- * idx: index of the list where the new node should be added.
+ * @idx: index of the list where the new node should be added.
+ * @n: value to be inserted
  * Return:  address of the new node, or NULL if it failed
  */
 
@@ -30,13 +31,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	for (d = 0; temp && d < idx; d++)
 	{
 		if (d == idx - 1)
-			{
-				new->next = temp->next;
-				temp->next = new;
-				return (new);
-			}
-			else
-				temp = temp->next;
+		{
+			new->next = temp->next;
+			temp->next = new;
+			return (new);
+		}
+		else
+			temp = temp->next;
 	}
 	return (NULL);
 }
